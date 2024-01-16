b2a10ec5383d0816a3c32bc29f3c321d0e315712
jo-dennehy <joanna.dennehy@gmail.com>
2024-01-16 16:18:21 +0000

    add htseq-count

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -21,3 +21,10 @@ cutadapt -a GATCGGAAGAGCACACGTCTGAACTCCAGTCACGGATGACTATCTCGTATGCCGTCTTCTGCTTG -g
 hisat2-build GCF_000002655.1_ASM265v1_genomic.fna genomic_ref
 
 # Hisat2 for alignment
+hisat2 -x ../../genomic_ref -1 NaCl_0_NT_A_EKRN230050117-1A_H7MFJDSX7_L3_1.fq -2 NaCl_0_NT_A_EKRN230050117-1A_H7MFJDSX7_L3_2.fq -S alignment.sam
+
+# Samtools to change .sam output to .bam
+samtools view -bS -o alignment.bam alignment.sam
+
+# htseq-counts
+htseq-count -f bam -r pos -s reverse -t exon -i gene_id -a 10 -m union --additional-attr gene_name alignment.sam ../../FungiDB-65_AfumigatusAf293.gff > counts.txt
